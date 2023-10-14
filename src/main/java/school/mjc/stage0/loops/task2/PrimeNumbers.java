8c0cfd9a86f65ee10672fe5a8ea17a0abe3acaa9
Shoghik <shoghik.kachatryan@dci-student.org>
2023-10-15 00:27:16 +0200

    first commit

--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -2,5 +2,20 @@ package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
+        int i = 0;
+        while(i <= printToInclusive) {
+            int j = 1;
+            int count = 0;
+            while (j <= i / 2) {
+                if (i % j == 0) {
+                    ++count;
+                }
+                ++j;
+            }
+            if (count == 1) {
+                System.out.println(i);
+            }
+            ++i;
+        }
     }
 }
